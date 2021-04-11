a15dceaeecea853657065e80b3e98b30ebefa064
Travis CI <najwer23@gmail.com>
2021-04-11 08:14:11 +0200

    CSS in webpack

---
+++ assets/js/style.js
@@ -0,0 +1,2 @@
+import '../css/muli.css';
+import '../css/index.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
-
 const path = require('path')
 
 module.exports = {
   entry: {
     app: [
       '@babel/polyfill', 
-      './assets/js/index.js',
-      './assets/js/test.js'
+      './assets/js/index.js'
+    ],
+    styleMain: [
+      './assets/js/style.js'
     ]
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'index.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      },
       {
         test: /\.js?$/,
         exclude: /node_modules/,
@@ -22,9 +31,13 @@ module.exports = {
         query: {
           presets: ['@babel/preset-env']
         }
+      },
+      {
+        test: /\.css?$/,
+        loader: 'style-loader!css-loader'
       }
     ]
   }
 }
 
-// multi entry, multi-out
+// https://github.com/webpack/webpack/issues/1732
