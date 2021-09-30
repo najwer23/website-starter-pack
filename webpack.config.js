d01b701fdda43dd0d295ca42d22465d0ceef90e5
Travis CI <najwer23@gmail.com>
2021-09-30 09:14:00 +0200

    Reconfigure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
+
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: [
-      '@babel/polyfill',
-      './assets/js/index.js'
-    ],
-    styleMain: [
-      './assets/js/style.js'
-    ]
+    index: path.resolve(__dirname, './assets/js/index.js'),
+    style: path.resolve(__dirname, './assets/js/style.js') 
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].bunde.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: "./assets/img/f.png",
+    })
+  ],
   module: {
     rules: [
       {
@@ -26,19 +29,17 @@ module.exports = {
         }
       },
       {
-        test: /\.(sass|scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: ['file-loader']
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: ['file-loader']
-      }
+        test: /\.(sass|scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(?:ico|gif|svg|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
     ]
-  }
+  },
 }
-
-// https://github.com/webpack/webpack/issues/1732
