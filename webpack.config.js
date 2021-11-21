0830fe124a812776da96b6a70c3b95e1b05fa5af
Mariusz Najwer <najwer23@gmail.com>
2021-11-21 20:05:24 +0100

    Bug with loading css after js. mini css on production, added loading files into build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
 
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var entryCustom = {};
+let entryArr = ["style", "index"];
+for (let i = 0; i < entryArr.length; i++) {
+  entryCustom[entryArr[i]] = path.resolve(__dirname, './assets/js/' + entryArr[i] + '.js')
+}
 
 module.exports = {
-  entry: {
-    index: path.resolve(__dirname, './assets/js/index.js'),
-    style: path.resolve(__dirname, './assets/js/style.js') 
-  },
+  entry: entryCustom,
   output: {
     path: path.resolve(__dirname, './build'),
     filename: '[name].bunde.js'
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          type: "css/mini-extract",
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
+    minimizer: [
+      '...',
+    ],
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      linkType: "text/css",
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "index.html"),
       favicon: "./assets/img/f.png",
@@ -34,11 +56,21 @@ module.exports = {
       },
       {
         test: /\.(sass|scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader", 'sass-loader'],
       },
       {
         test: /\.(?:ico|gif|svg|png|jpg|jpeg)$/i,
         type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/img/',
+              publicPath: 'assets/img/'
+            }
+          }
+        ]
       },
     ]
   },
